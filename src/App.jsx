69abbf2723ccc05606184c88e8577156f1d9c6be
Meaza Berhe <meazaberhe38@gmail.com>
2026-05-21 16:57:48 -0400

    updated app css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./assets/Component/Header/Header";
 import Banner from "./assets/Component/Banner/Banner";
 import DisplayRow from "./assets/Component/DisplayRow/DisplayRow";
 // import MovieCard from "./assets/Component/MovieCard/MovieCard";
-import "./app.css";
+import "./App.css";
 import Footer from "./assets/Component/Footer/Footer";
 function App() {
   return (
